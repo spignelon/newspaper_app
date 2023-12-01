418cc74de290f9e495bd7c296ab794a23f172127
Ujjawal Saini <spignelon@protonmail.com>
2023-12-01 14:36:03 +0530

    commited the project

---
+++ accounts/tests.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+class SignUpPageTests(TestCase):
+    def test_url_exists_at_current_location_signupview(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
+    def test_signup_view_name(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/signup.html")
+    def test_signup_form(self):
+        response = self.client.post(
+            reverse("signup"),
+            {
+                "username": "testuser",
+                "email": "testuser@testing.org",
+                "password1": "testpass123",
+                "password2": "testpass123",
+            }
+        )    
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, "testuser")
+        self.assertEqual(get_user_model().objects.all()[0].email, "testuser@testing.org")
---
+++ articles/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Article, Comment
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
